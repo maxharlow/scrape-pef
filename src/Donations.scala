6a1b41dd6169c66fc6a431a404aa19a6e06d2f6c
Maxwell Harlow <maxharlow@gmail.com>
2014-03-16 19:45:57 +0000

    Clean separately

--- src/Donations.scala
+++ src/Donations.scala
@@ -7,8 +7,7 @@ import CypherTools._
 
 object Donations {
 
-  implicit class CypherParameterValue(v: String) {
-    val value = v.trim.filter(_ >= ' ').replace("""\""", """\\""").replace("'", """\'""")
+  implicit class CypherParameterValue(value: String) {
     def int = Option(value.replaceAll("[^0-9]", "")).filter(_.nonEmpty)
     def string = if (value.isEmpty) None else Some("'" + value + "'")
     def boolean = Some((!value.isEmpty).toString)
@@ -36,36 +35,36 @@ object Donations {
 
   def getBenefactor(entry: Map[String, String]): Map[String, Option[String]] = {
     Map(
-      "name" -> stripTitles(entry("Donor name")).string,
-      "benefactorType" -> stripTitles(entry("Donor type")).string,
-      "postcode" -> entry("Postcode").string, // optional
-      "companyNumber" -> entry("Company reg. no.").replaceAll("[^0+A-Za-z0-9]", "").replaceAll("^0*", "").string // optional
+      "name" -> stripTitles(clean(entry("Donor name"))).string,
+      "benefactorType" -> clean(entry("Donor type")).string,
+      "postcode" -> clean(entry("Postcode")).string, // optional
+      "companyNumber" -> clean(entry("Company reg. no.").replaceAll("[^0+A-Za-z0-9]", "").replaceAll("^0*", "")).string // optional
     )
   }
 
   def getRecipient(entry: Map[String, String]): Map[String, Option[String]] = {
     Map(
-      "name" -> stripTitles(entry("Entity name")).string,
-      "recipientType" -> entry("Entity type").string,
-      "recipientRegulatedType" -> entry("Regulated donee type").string // optional
+      "name" -> stripTitles(clean(entry("Entity name"))).string,
+      "recipientType" -> clean(entry("Entity type")).string,
+      "recipientRegulatedType" -> clean(entry("Regulated donee type")).string // optional
     )
   }
 
   def getDonation(entry: Map[String, String]): Map[String, Option[String]] = {
     Map(
-      "ecReference" -> entry("EC reference").string,
-      "type" -> entry("Type of donation").string,
-      "value" -> entry("Value").int, // in pence
-      "acceptedDate" -> entry("Accepted date").date,
-      "receivedDate" -> entry("Received date").date, // optional
-      "reportedDate" -> entry("Reported date").date, // optional
-      "nature" -> entry("Nature / Provision").string, // optional
-      "purpose" -> entry("Purpose").string, // optional
-      "howDealtWith" -> entry("How dealt with").string, // optional
-      "recordedBy" -> entry("Rec'd by (AU)").string, // optional
-      "reportedUnder6212" -> entry("Reported under 62:12").string, // optional
-      "isSponsorship" -> entry("Is sponsorship").boolean,
-      "complianceBreach" -> entry("Compliance breach").string
+      "ecReference" -> clean(entry("EC reference")).string,
+      "type" -> clean(entry("Type of donation")).string,
+      "value" -> clean(entry("Value")).int, // in pence
+      "acceptedDate" -> clean(entry("Accepted date")).date,
+      "receivedDate" -> clean(entry("Received date")).date, // optional
+      "reportedDate" -> clean(entry("Reported date")).date, // optional
+      "nature" -> clean(entry("Nature / Provision")).string, // optional
+      "purpose" -> clean(entry("Purpose")).string, // optional
+      "howDealtWith" -> clean(entry("How dealt with")).string, // optional
+      "recordedBy" -> clean(entry("Rec'd by (AU)")).string, // optional
+      "reportedUnder6212" -> clean(entry("Reported under 62:12")).string, // optional
+      "isSponsorship" -> clean(entry("Is sponsorship")).boolean,
+      "complianceBreach" -> clean(entry("Compliance breach")).string
     )
   }
 
@@ -94,6 +93,9 @@ object Donations {
     if (!result) println(" => failed to add donation")
   }
 
+  def clean(text: String): String = {
+    text.filter(_ >= ' ').replace("""\""", """\\""").replace("'", """\'""").trim
+  }
 
   def stripTitles(name: String): String = {
     val prefixes = List("Ms", "Mrs", "Miss", "Mr", "Dr", "Lord", "Baron", "Baroness", "Cllr", "Sir", "Dame", "The Hon", "The Rt Hon")
