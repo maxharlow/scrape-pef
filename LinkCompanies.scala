ad5be3fa95a96d49a2a8ff92c9c7f8caad0376e7
Max Harlow <maxharlow@gmail.com>
2014-08-05 17:35:40 +0100

    Refactor companies link stuff

--- LinkCompanies.scala
+++ LinkCompanies.scala
@@ -1,112 +1,127 @@
 import java.io.File
-import scala.util.{Try, Success, Failure}
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.collection.immutable.ListMap
+import dispatch._
+import dispatch.Defaults._
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import org.json4s.DefaultFormats
-import org.json4s.JValue
+import org.json4s.{JValue, JString, JBool, JNull}
 import org.json4s.native.JsonMethods
 import org.anormcypher.{Cypher, Neo4jREST}
-import CommonCypher._
-import CommonUtils._
 
 object LinkCompanies extends App {
 
-  Neo4jREST.setServer("localhost")
+  type JsonMap = ListMap[String, JValue]
 
   implicit val formats = DefaultFormats
-  val opencorporatesApiToken = ""
+  val opencorporatesApiToken = "jvc5BuxuEBq2NK1Vgjom"
+
+  println("""
+    ___  __     __
+   / _ \/ /_ __/ /____
+  / ___/ / // / __/ _ \
+ /_/  /_/\_,_/\__/\___/
+
+  """)
+
+  Neo4jREST.setServer("localhost")
 
   run()
 
   def run() {
-    companyNumbers foreach { number =>
-      println(s"Updating data for company $number...")
-      companyData(number) match {
-        case Failure(e) => println(s" => failed to get data (${e.getMessage.toLowerCase})")
-        case Success(companyJson) => {
-          val company = getCompany(companyJson)
-          addCompany(company, number)
-          (companyJson \ "officers").children map { officerObject =>
-            val officerJson = (officerObject \ "officer")
-            val officer = getOfficer(officerJson)
-            val officerName = officer.values("name").get.init.tail // unquoted
-            val officership = getOfficership(officerJson)
-            addOfficer(officer)
-            addOfficership(officership, officerName, number)
-          }
-        }
+    for (companyNumber <- companyNumbers)
+    yield for (response <- retrieve(companyNumber)) {
+      val company = selectCompany(response)
+      val companyOfficers = for (officers <- (response \ "officers").children)
+      yield {
+        val officerJson = officers \ "officer"
+        val officer = selectOfficer(officerJson)
+        val officership = selectOfficership(officerJson)
+        (officer, officership)
+      }
+      load(companyNumber, company, companyOfficers)
+    }
+  }
+
+  def companyNumbers: List[String] = {
+    val query = Cypher("MATCH (n) WHERE has(n.companyNumber) RETURN n.companyNumber AS number").apply()
+    query.map(_[String]("number")).toList
+  }
+
+  def retrieve(number: String): Future[JValue] = {
+    val response = Http {
+      url(s"https://api.opencorporates.com/companies/gb/$number?api_token=$opencorporatesApiToken") OK as.String
+    }
+    Await.ready(response, 1.minute)
+    response map { r => //todo: print message on failures
+      JsonMethods.parse(r) \\ "company"
+    }
+  }
+
+  def selectCompany(companyData: JValue): JsonMap = {
+    ListMap(
+      "companyName" -> companyData \ "name",
+      "companyType" -> companyData \ "company_type",
+      "companyRegisteredAddress" -> companyData \ "registered_address_in_full",
+      "companyIncorporationDate" -> companyData \ "incorporation_date",
+      "companyDissolutionDate" -> companyData \ "dissolution_date",
+      "companyStatus" -> companyData \ "current_status",
+      "companyInactive" -> companyData \ "inactive"
+    )
+  }
+
+  def selectOfficer(officerData: JValue): JsonMap = {
+    ListMap(
+      "name" -> officerData \ "name"
+    )
+  }
+
+  def selectOfficership(officerData: JValue): JsonMap = {
+    ListMap(
+      "position" -> officerData \ "position",
+      "startDate" -> officerData \ "start_date",
+      "endDate" -> officerData \ "end_date"
+    )
+  }
+
+  def propertise(bind: String, jsonMap: JsonMap): String = {
+    val pairs = jsonMap map {
+      case (key, JString(value)) if key contains "Date" => s"""\n  $bind.$key=${value.replace("-", "")}"""
+      case (key, JString(value)) => s"\n  $bind.$key='${clean(value)}'"
+      case (key, JBool(value)) => s"\n  $bind.$key=${value.toString}"
+      case (key, JNull) => ""
+      case _ => throw new Exception("Unexpected Json!")
+    }
+    pairs.filter(!_.isEmpty).mkString(",")
+  }
+
+  def load(companyNumber: String, company: JsonMap, officers: Seq[(JsonMap, JsonMap)]): Unit = {
+    val companyProps = propertise("c", company)
+    val query = {
+      s"MATCH (c:Organisation {companyNumber:'$companyNumber'}) SET $companyProps" +
+      officers.zipWithIndex.foldLeft("\n") { case (a, ((officer, officership), i)) =>
+        val officerProps = propertise("o" + i, officer)
+        val officershipProps = propertise("iaoo" + i, officership)
+        val officerName = clean(officer("name").extract[String])
+        a + s"""
+        |MERGE (o$i:Individual {name: '$officerName'}) SET $officerProps
+        |MERGE (o$i)-[iaoo$i:IS_AN_OFFICER_OF]->(c) SET $officershipProps
+        """ stripMargin // todo fuzzy-match on name
       }
     }
-  }
-
-  private def companyNumbers: List[String] = {
-    val companyNumbersQuery = Cypher("MATCH (n) WHERE has(n.companyNumber) RETURN n.companyNumber as number").apply()
-    companyNumbersQuery.map(_[String]("number")).toList
-  }
-
-  private def companyData(number: String): Try[JValue] = {
-    request(s"https://api.opencorporates.com/companies/gb/$number?api_token=$opencorporatesApiToken") map { response =>
-      JsonMethods.parse(response) \\ "company"
+    val result = Cypher(query).execute()
+    if (!result) {
+      println(s"FAILED TO UPDATE COMPANY $companyNumber:\n")
+      println(query)
     }
   }
 
-  private def getCompany(companyJson: JValue): CypherObject = {
-    new CypherObject("Organisation")(
-      "companyName" -> extractString(companyJson \ "name").string,
-      "companyType" -> extractString(companyJson \ "company_type").string,
-      "companyRegisteredAddress" -> extractString(companyJson \ "registered_address_in_full").string,
-      "companyIncorporationDate" -> extractString(companyJson \ "incorporation_date").date("yyyy-MM-dd"),
-      "companyDissolutionDate" -> extractString(companyJson \ "dissolution_date").date("yyyy-MM-dd"),
-      "companyStatus" -> extractString(companyJson \ "current_status").string,
-      "companyInactive" -> extractBoolean(companyJson \ "inactive").boolean
-    )
-  }
-
-  private def addCompany(company: CypherObject, companyNumber: String): Unit = {
-    val companyProperties = company.toUpdateString("n")
-    val result = Cypher(s"MATCH (n {companyNumber:'$companyNumber'}) SET $companyProperties").execute()
-    if (!result) println(" => failed to add details for company")
-  }
-
-  private def getOfficer(officerJson: JValue): CypherObject = {
-    new CypherObject("Individual")(
-      "name" ->  titleCase(extractString(officerJson \ "name")).string
-    )
-  }
-
-  private def addOfficer(officer: CypherObject): Unit = {
-    val officerName = officer.values("name").get.init.tail // unquoted
-    if (Cypher(s"MATCH o WHERE o.name =~ '(?i).*$officerName.*' RETURN o").apply().isEmpty) {
-      val officerProperties = officer.toMatchString("o")
-      val result = Cypher(s"CREATE ($officerProperties)").execute()
-      if (!result) println(" => failed to add officer")
-    } // (no update otherwise)
-  }
-
-  private def getOfficership(officerJson: JValue): CypherObject = {
-    new CypherObject("OFFICER_OF")(
-      "position" -> extractString(officerJson \ "position").string,
-      "startDate" -> extractString(officerJson \ "start_date").date("yyyy-MM-dd"),
-      "endDate" -> extractString(officerJson \ "end_date").date("yyyy-MM-dd")
-    )
-  }
-
-  private def addOfficership(officership: CypherObject, officerName: String, companyNumber: String): Unit = {
-    val officershipProperties = officership.toMatchString()
-    val matchCypher = s"MATCH (o), (c:Organisation {companyNumber:'$companyNumber'}) WHERE o.name =~ '(?i).*$officerName.*'"
-    val mergeCypher = s"MERGE (o)-[$officershipProperties]->(c)"
-    val result = Cypher(s"$matchCypher $mergeCypher").execute()
-    if (!result) println(" => failed to add officership")
-  }
-
-  private def extractString(json: JValue): String = {
-    json.noNulls.toOption.map(_.extract[String]).getOrElse("")
-  }
-
-  private def extractBoolean(json: JValue): String = {
-    json.noNulls.toOption.map(_.extract[Boolean].toString).getOrElse("")
-  }
-
-  private def titleCase(text: String): String = {
-    text.split(" ").map(_.toLowerCase.capitalize).mkString(" ")
+  def clean(text: String): String = {
+    val escaped = text.replace("""\""", """\\\\""").replace("'", """\'""")
+    if (text contains ',') escaped
+    else escaped.split(" ").map(_.toLowerCase.capitalize).mkString(" ") // titlecase
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -7,6 +7,7 @@ resolvers ++= Seq(
 
 libraryDependencies ++= Seq(
   "joda-time" % "joda-time" % "2.3",
+  "net.databinder.dispatch" %% "dispatch-core" % "0.11.1",
   "org.json4s" %% "json4s-native" % "3.2.10",
   "com.github.tototoshi" %% "scala-csv" % "1.0.0",
   "org.anormcypher" %% "anormcypher" % "0.5.1"
