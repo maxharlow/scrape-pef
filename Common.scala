796e8e538859f1a2b4c9c901f13e5622d3cd0673
Max Harlow <maxharlow@gmail.com>
2014-11-11 17:26:43 +0000

    Unused code

--- Common.scala
+++ Common.scala
@@ -6,21 +6,8 @@ import org.json4s.{JValue, JString, JBool, JNull}
 
 object Common {
 
-  def tidy(text: String): String = {
-    val escaped = escapeCypher(clean(text))
-    if (text contains ',') escaped else titlecase(escaped) // attempt to only titlecase sensible things
-  }
-
   def clean(text: String): String = {
     text.filter(_ >= ' ').trim.replaceAll(" +", " ").replaceAll("\\.", "")
-  }
-
-  def escapeCypher(text: String): String = {
-    text.replaceAll("""[()\[\]\*\\'"]""", """\$0""")
-  }
-
-  def titlecase(text: String): String = {
-    text.split(" ").map(_.toLowerCase.capitalize).mkString(" ")
   }
 
   def stripTitles(name: String): String = {
@@ -35,23 +22,6 @@ object Common {
     postcode.replaceAll("ZZ0 0ZZ|ZZ00ZZ|ZZ1 1ZZ|ZZ11ZZ|AA0 0AA|AA00AA|AA1 1AA|AA11AA", "")
   }
 
-  def nameCheck[A](name: String)(check: String => Try[A]): Try[A] = {
-    val names = name.split(" ")
-    val middleNames = if (names.length > 2) names.init.tail.toList else Nil
-
-    def shortName = check(names.head + " " + names.last)
-    def initalledName = check(names.head + middleNames.map(_.head).mkString(" ") + names.last)
-    def initalledDottedName = check(names.head + middleNames.map(_.head).mkString(". ") + names.last)
-
-    val fullNameCheck = check(name)
-
-    if (fullNameCheck.isSuccess) fullNameCheck
-    else if (!middleNames.isEmpty) {
-      shortName orElse initalledName orElse initalledDottedName
-    }
-    else fullNameCheck // if all else fails
-  }
-
   def asDate(value: String, format: String): String = {
     val formatPattern = DateTimeFormat.forPattern(format)
     if (value.isEmpty) ""
@@ -64,17 +34,6 @@ object Common {
 
   def asBoolean(value: String): String = {
     if (value.isEmpty) "false" else "true"
-  }
-
-  def propertise(bind: String, jsonMap: ListMap[String, JValue]): String = {
-    val pairs = jsonMap map {
-      case (key, JString(value)) if key contains "Date" => s"""\n  $bind.$key=${value.replace("-", "")}"""
-      case (key, JString(value)) => s"$bind.$key='${tidy(value)}'"
-      case (key, JBool(value)) => s"$bind.$key=${value.toString}"
-      case (key, JNull) => ""
-      case _ => throw new Exception("Unexpected Json!")
-    }
-    pairs.filter(!_.isEmpty).mkString(",")
   }
 
 }
