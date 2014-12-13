9cf035cc17272f556c1929d7897c96060b90564d
Max Harlow <maxharlow@gmail.com>
2014-12-13 20:07:36 +0000

    Update loans

--- pef-loans.scala
+++ pef-loans.scala
@@ -6,30 +6,47 @@ import com.gargoylesoftware.htmlunit.html._
 object Loans extends PEF {
 
   override val headers = List(
-    "benefactorClass",
-    "benefactorName",
-    "benefactorType",
-    "benefactorAddress",
-    "benefactorPostcode",
-    "benefactorCompanyNumber",
-    "recipientClass",
-    "recipientName",
-    "recipientDeregisteredDate",
-    "recipientType",
     "ecReference",
-    "type",
+    "ecLastNotifiedDate",
+    "ecPublishedDate",
+    "ecReleaseTitle",
+    "lenderType",
+    "lenderTitle",
+    "lenderFirstName",
+    "lenderMiddleName",
+    "lenderLastName",
+    "lenderName",
+    "lenderCompanyNumber",
+    "lenderAddress",
+    "lenderPostcode",
+    "lenderCountry",
+    "lenderPhoneNumber",
+    "lenderEmailAddress",
+    "recipientName",
+    "recipientID",
+    "recipientType",
+    "recipientRegulatedType",
+    "recipientDeregisteredDate",
+    "accountingUnit",
+    "accountingUnitID",
     "value",
-    "referenceNumber",
+    "valueRepaid",
+    "valueConverted",
+    "valueOutstanding",
+    "type",
     "rate",
+    "rateFixed",
+    "rateVariable",
     "status",
-    "amountRepaid",
-    "amountConverted",
-    "amountOutstanding",
+    "repaymentTerm",
     "startDate",
     "endDate",
     "repaidDate",
-    "ecLastNotifiedDate",
-    "recordedBy"
+    "referenceNumber",
+    "notes",
+    "additionalInformation",
+    "isReportedDueToAggregation",
+    "hasSecurityBeenGiven"
   )
 
   override val controlSearch = "ctl00$ctl05$ctl07"
@@ -37,99 +54,132 @@ object Loans extends PEF {
 
   run("pef-loans.csv")
 
-  // override def lookupList(response: HtmlPage): Map[String, String] = {
-  //   ListMap(
-  //     "organisation" -> response.getElementByName[HtmlSelect]("ctl00_ContentPlaceHolder1_LoanTransactionControl1_loanTransactionControl1_ddlOrganisation").getTextContent(),
-  //     "reference" -> response.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$txtReference").getTextContent(),
-  //     "enteredIntoDate" -> {
-  //       val year = response.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$dsDateEnteredInto$ddlYear").getTextContent()
-  //       val month = response.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$dsDateEnteredInto$ddlMonth").getTextContent()
-  //       val day = response.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$dsDateEnteredInto$ddlDay").getTextContent()
-  //       s"$year-$month-$day"
-  //     },
-  //     "repaymentTerm" -> response.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$ddlLoanRepaymentTerm").getTextContent(),
-  //     "repayableDate" -> {
-  //       val year = response.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$dsDateRepayable$ddlYear").getTextContent()
-  //       val month = response.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$dsDateRepayable$ddlMonth").getTextContent()
-  //       val day = response.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$dsDateRepayable$ddlDay").getTextContent()
-  //       val date = s"$year-$month-$day"
-  //       if (date != "----------") date else ""
-  //     },
-  //     "value" -> response.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$txtLoanValue").getTextContent(),
-  //     "interestRate" -> response.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$txtRateOfInterest").getTextContent(),
-  //     "interestRateFixed" -> response.getElementByName[HtmlCheckBoxInput]("ctl00_ContentPlaceHolder1_LoanTransactionControl1_loanTransactionControl1_chkRateOfInterestFixed").isChecked().toString(),
-  //     "interestRateVariable" -> response.getElementByName[HtmlCheckBoxInput]("ctl00_ContentPlaceHolder1_LoanTransactionControl1_loanTransactionControl1_chkRateOfInterestVariable").isChecked().toString(),
-  //     "additionalInformation" -> response.getElementByName[HtmlTextArea]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$txtAdditionalInformation").getTextContent(),
-  //     "benefactorStatus" -> response.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$ddlParticipantStatus").getTextContent(),
-  //     "benefactorTitle" -> {
-  //       val title = response.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$ddlTitle").getTextContent()
-  //       if (title contains "Please Select") "" else title
-  //     },
-  //     "benefactorFirstName" -> {
-  //       val firstName = response.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtFirstname").getTextContent()
-  //       if (firstName == "na") "" else firstName
-  //     },
-  //     "benefactorMiddleName" -> response.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtMiddlename").getTextContent(),
-  //     "benefactorLastName" -> response.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtSurname").getTextContent(),
-  //     "benefactorCompanyName" -> {
-  //       response.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtParticupantName").getTextContent()
-  //     }
-  //   )
-  // }
-
   override def select(record: Map[String, String], page: HtmlPage): Map[String, String] = {
     ListMap(
-      "benefactorClass" -> {
-        val benefactorType = record("Lender type")
-        if (benefactorType == "Individual") "Individual"
-        else if (benefactorType == "Registered Political Party") "Party"
-        else "Organisation"
+      "ecReference" -> record("EC reference"),
+      "ecLastNotifiedDate" -> asDate(record("Date EC last notified"), "dd/MM/yyyy"),
+      "ecPublishedDate" -> {
+        val publishedDate = Try(page.getElementById[HtmlSpan]("ctl00_ContentPlaceHolder1_LoanTransactionControl1_lblPublishedDateValue", true))
+        publishedDate.map(_.getTextContent()).map(asDate(_, "dd/MM/yyyy")).getOrElse("")
       },
-      "benefactorName" -> {
-        val name = record("Lender name").replaceAll(", ([0-9]),", ", $1")
-        if (record("Lender type") == "Individual") stripTitles(name)
-        else if (name contains ", ") name.split(", ").init.mkString(", ") // split from address
-        else name
+      "ecReleaseTitle" -> page.getElementById[HtmlSpan]("ctl00_ContentPlaceHolder1_LoanTransactionControl1_lblTransactionTitle", true).getTextContent(),
+      "lenderType" -> record("Lender type"),
+      "lenderTitle" -> { // individuals only
+        val lenderTitle = Try(page.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$ddlTitle"))
+        val lenderTitleText = lenderTitle.map(_.getSelectedOptions().head.getTextContent()).getOrElse("")
+        if (lenderTitleText matches "na|-- Please Select --") "" else lenderTitleText
       },
-      "benefactorType" -> record("Lender type"),
-      "benefactorAddress" -> {
-        val name = record("Lender name").replaceAll(", ([0-9]),", ", $1")
-        if (record("Lender type") == "Individual") ""
-        else if (name contains ", ") name.split(", ").last.replaceAll("^(A)$|^(NA)$", "") // split from name
-        else ""
+      "lenderFirstName" -> { // individuals only
+        val lenderFirstName = Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtFirstname"))
+        val lenderFirstNameText = lenderFirstName.map(_.getValueAttribute()).getOrElse("")
+        if (lenderFirstNameText == "na") "" else lenderFirstNameText
       },
-      "benefactorPostcode" -> stripFakePostcodes(record("Postcode")), // optional
-      "benefactorCompanyNumber" -> {
-        val benefactorType = record("Lender type")
-        if (benefactorType == "Registered Political Party") "" // parties shouldn't have company numbers
-        else record("Company reg. no.").replaceAll("[^0+A-Za-z0-9]", "").replaceAll("^0*", "") // optional
+      "lenderMiddleName" -> { // individuals only
+        val lenderMiddleName = Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtMiddlename"))
+        val lenderMiddleNameText = lenderMiddleName.map(_.getValueAttribute()).getOrElse("")
+        if (lenderMiddleNameText == "na") "" else lenderMiddleNameText
       },
-      "recipientClass" -> {
-        val recipientType = record("Entity type")
-        if (recipientType == "Political Party" || recipientType == "Third Party") "Party"
-        else "Organisation" // cannot loan to individuals
+      "lenderLastName" -> { // individuals only
+        val lenderLastName = Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtSurname"))
+        val lenderLastNameText = lenderLastName.map(_.getValueAttribute()).getOrElse("")
+        if (lenderLastNameText == "na") "" else lenderLastNameText
       },
-      "recipientName" -> stripTitles(record("Entity name")).replaceAll(""" \[De-registered .*\]""", "").replaceAll("Conservative and Unionist Party", "Conservative Party"),
-      "recipientDeregisteredDate" -> { // optional, de-registered parties only
+      "lenderName" -> { // non-individuals only
+        val lenderName = Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtParticupantName"))
+        val lenderNameText = lenderName.map(_.getValueAttribute()).getOrElse("")
+        if (lenderNameText == "na") "" else lenderNameText
+      },
+      "lenderCompanyNumber" -> {
+        val lenderCompanyNumber = Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtCompanyRegistrationNumber"))
+        lenderCompanyNumber.map(_.getValueAttribute()).getOrElse("")
+      },
+      "lenderAddress" -> {
+        val lenderAddress = List(
+          Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtAddressLine1")).map(_.getValueAttribute()),
+          Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtAddressLine2")).map(_.getValueAttribute()),
+          Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtAddressLine3")).map(_.getValueAttribute()),
+          Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtAddressLine4")).map(_.getValueAttribute()),
+          Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtTown")).map(_.getValueAttribute()).map(_.replace("no town", "")),
+          Try(page.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$ddlCounty")).map(_.getSelectedOptions().head.getTextContent())
+        )
+        lenderAddress.map(_.getOrElse("")).filterNot(_.isEmpty).mkString(", ")
+      },
+      "lenderPostcode" -> {
+        val lenderPostcode = Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtPostcode"))
+        lenderPostcode.map(_.getValueAttribute()).map(stripFakePostcodes).getOrElse("")
+      },
+      "lenderCountry" -> {
+        val lenderCountry = Try(page.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$ddlCountry"))
+        val lenderCountryText = lenderCountry.map(_.getSelectedOptions().head.getTextContent()).getOrElse("")
+        if (lenderCountryText contains "Please Select") "" else lenderCountryText
+      },
+      "lenderPhoneNumber" -> {
+        val lenderPhoneNumber = Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtPhoneNumber"))
+        lenderPhoneNumber.map(_.getValueAttribute()).getOrElse("")
+      },
+      "lenderEmailAddress" -> {
+        val lenderEmailAddress = Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$participant1$txtEmail"))
+        lenderEmailAddress.map(_.getValueAttribute()).getOrElse("")
+      },
+      "recipientName" -> {
+        val recipientName = stripTitles(record("Entity name")).replaceAll(""" \[De-registered .*\]""", "")
+        recipientName.replaceAll("Conservative and Unionist Party", "Conservative Party")
+      },
+      "recipientID" -> record("Entity ID"),
+      "recipientType" -> record("Entity type"),
+      "recipientRegulatedType" -> {
+        val recipientRegulatedType = Try(page.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$ddlRegulatedDoneeStatus"))
+        recipientRegulatedType.map(_.getSelectedOptions().head.getTextContent()).getOrElse("")
+      },
+      "recipientDeregisteredDate" -> { // de-registered parties only
         val recipientName = record("Entity name")
         if (recipientName contains "De-registered") asDate(recipientName.replaceAll(".*De-registered ", ""), "dd/MM/yy]")
         else ""
       },
-      "recipientType" -> record("Entity type"),
-      "ecReference" -> record("EC reference"),
+      "accountingUnit" -> { // aka 'organisation entering into the transaction'
+        val accountingUnit = Try(page.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$ddlOrganisation"))
+        accountingUnit.map(_.getSelectedOptions().head.getTextContent()).getOrElse("")
+      },
+      "accountingUnitID" -> record("Accounting unit ID"),
+      "value" -> record("Total amount"),
+      "valueRepaid" -> record("Amount repaid"),
+      "valueConverted" -> record("Amount converted"),
+      "valueOutstanding" -> record("Amount outstanding"),
       "type" -> record("Type of borrowing"),
-      "value" -> asInt(record("Total amount").dropRight(2)), // in pence (to four decimal places...?)
-      "referenceNumber" -> record("Loan reference no."), // optional
-      "rate" -> record("Rate"), // optional
+      "rate" -> record("Rate"),
+      "rateFixed" -> {
+        val rateFixed = Try(page.getElementByName[HtmlCheckBoxInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$chkRateOfInterestFixed"))
+        rateFixed.map(_.isChecked().toString()).getOrElse("")
+      },
+      "rateVariable" -> {
+        val rateVariable = Try(page.getElementByName[HtmlCheckBoxInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$chkRateOfInterestVariable"))
+        rateVariable.map(_.isChecked().toString()).getOrElse("")
+      },
       "status" -> record("Status"),
-      "amountRepaid" -> asInt(record("Amount repaid").dropRight(2)),
-      "amountConverted" -> asInt(record("Amount converted").dropRight(2)),
-      "amountOutstanding" -> asInt(record("Amount outstanding").dropRight(2)),
-      "startDate" -> asDate(record("Start date"), "dd/MM/yyyy"),
-      "endDate" -> asDate(record("End date"), "dd/MM/yyyy"), // optional
-      "repaidDate" -> asDate(record("Date repaid"), "dd/MM/yyyy"), // optional
-      "ecLastNotifiedDate" -> asDate(record("Date EC last notified"), "dd/MM/yyyy"),
-      "recordedBy" -> record("Rec'd by (AU)") // optional
+      "repaymentTerm" -> {
+        val repaymentTerm = Try(page.getElementByName[HtmlSelect]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$ddlLoanRepaymentTerm"))
+        repaymentTerm.map(_.getSelectedOptions().head.getTextContent()).getOrElse("")
+      },
+      "startDate" -> asDate(record("Start date"), "dd/MM/yyyy"), // aka 'entered into date'
+      "endDate" -> asDate(record("End date"), "dd/MM/yyyy"), // aka 'repayable date', 'end/review date'
+      "repaidDate" -> asDate(record("Date repaid"), "dd/MM/yyyy"),
+      "referenceNumber" -> record("Loan reference no."),
+      "notes" -> {
+        val notes = Try(page.getElementByName[HtmlInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$txtNatureOfTransactionNotes"))
+        notes.map(_.getValueAttribute()).getOrElse("")
+      },
+      "additionalInformation" -> {
+        val additionalInformation = Try(page.getElementByName[HtmlTextArea]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$txtAdditionalInformation"))
+        additionalInformation.map(_.getTextContent()).getOrElse("")
+      },
+      "isReportedDueToAggregation" -> {
+        val isReportedDueToAggregation = Try(page.getElementByName[HtmlCheckBoxInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$chkReportedBecauseOfAggregation"))
+        isReportedDueToAggregation.map(_.isChecked().toString()).getOrElse("")
+      },
+      "hasSecurityBeenGiven" -> {
+        val hasSecurityBeenGiven = Try(page.getElementByName[HtmlCheckBoxInput]("ctl00$ContentPlaceHolder1$LoanTransactionControl1$loanTransactionControl1$chkHasSecurityBeenGiven"))
+        hasSecurityBeenGiven.map(_.isChecked().toString()).getOrElse("")
+      }
     )
   }
 
